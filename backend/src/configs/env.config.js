e6c8aebb837dd7084514e07c0112257766059900
SankalpPimpalkar <pimpalkarsankalp8@gmail.com>
2025-12-20 19:16:37 +0530

    updated env configuration [BUG]

--- backend/src/configs/env.config.js
+++ backend/src/configs/env.config.js
@@ -7,6 +7,9 @@ const ENV = {
     DB_URL: process.env.DB_URL,
     INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
     CLIENT_URL: process.env.CLIENT_URL,
+    CLOUDINARY_CLOUDNAME: process.env.CLOUDINARY_CLOUDNAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 }
 
 export default ENV
